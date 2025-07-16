d9cd1eda90f4bdcc431def5b559bf2ca31a95dbf
mays benyoussef <maysbenyoussef@gmail.com>
2025-07-16 13:27:27 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,116 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let passwordLength = 12;
+
+function getRandomCharacter() {
+  let randomChar = Math.floor(Math.random() * characters.length);
+  return characters[randomChar];
+}
+
+function generateRandomPassword() {
+  let randomPassword = "";
+  for (let i = 0; i < passwordLength; i++) {
+    randomPassword += getRandomCharacter();
+  }
+
+  console.log("Here is a random password: ", randomPassword);
+  return randomPassword;
+}
+function clickGenerate() {
+  let cube1 = document.getElementById("cube1");
+  let cube2 = document.getElementById("cube2");
+  cube1.textContent = generateRandomPassword();
+  cube2.textContent = generateRandomPassword();
+}
